7bde26c4c651428e25f823a8582bc8c0dd71f03d
jtamary <tamary@gmail.com>
2014-05-03 12:47:48 +0300

    db support

--- web.js
+++ web.js
@@ -8,47 +8,48 @@ var DATABASE_URL = "postgres://mfayyxxtnqmcfv:O0MGIG-aFROUMgEPwjOQUddvl-@ec2-54-
 
 
 var app = express();
-app.get('/hello.txt', function(req, res){
+app.get('/', function(req, res){
     res.send('Hello World');
 });
 
 app.get('/add/:res', function(req, res){
     console.log('userAgent: ' + req.headers['user-agent']);
     console.log(req.param('res'));
-    res.send('added');
     var date = new Date();
     var agent = req.headers['user-agent'];
 
     pg.connect(process.env.DATABASE_URL, function(err, client) {
-        client.query('INSERT INTO pg_equipment(agent, time, date) VALUES($1,$2,$3)', [agent, req.param('res'), date]);
-        query.on('end', function(row) {
+        var query = client.query('INSERT INTO pg_equipment(agent, time, date) VALUES($1,$2,$3)', [agent, req.param('res'), date]);
+        query.on('end', function() {
             res.send('record added');
             client.end()
         });
+        query.on('error', function(row) {
+
     });
 
 
 
 });
 
-app.get('/install', function(req, res){
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
-
-        var query = client.query('CREATE TABLE pg_equipment ( \
-            id serial PRIMARY KEY,\
-                agent varchar (400) NOT NULL, \
-                time int NOT NULL, \
-                date date NOT NULL \
-            );'
-        );
-        query.on('end', function(row) {
-            console.log("sddd");
-            client.end()
-            res.send('table created');
-        });
-    });
-
-});
+//app.get('/install', function(req, res){
+//    pg.connect(process.env.DATABASE_URL, function(err, client) {
+//
+//        var query = client.query('CREATE TABLE pg_equipment ( \
+//            id serial PRIMARY KEY,\
+//                agent varchar (400) NOT NULL, \
+//                time int NOT NULL, \
+//                date date NOT NULL \
+//            );'
+//        );
+//        query.on('end', function() {
+//            console.log("sddd");
+//            client.end()
+//            res.send('table created');
+//        });
+//    });
+//
+//});
 
 
 var port = Number(process.env.PORT || 5000);
