cb362e7ab29096c3c404b0557954aa2251359de9
jtamary <tamary@gmail.com>
2014-05-07 10:43:48 +0300

    insert new table

--- web.js
+++ web.js
@@ -15,7 +15,8 @@ app.get('/', function(req, res){
 app.get('/add/:res', function(req, res){
     console.log('userAgent: ' + req.headers['user-agent']);
     console.log(req.param('res'));
-    var date = new Date();
+    var now = new Date();
+    var date = now.format("dd/M/yy h:mm tt");
     var agent = req.headers['user-agent'];
 
     pg.connect(process.env.DATABASE_URL, function(err, client) {
@@ -40,24 +41,72 @@ app.get('/res', function(req, res){
 });
 
 
-//app.get('/install', function(req, res){
-//    pg.connect(process.env.DATABASE_URL, function(err, client) {
-//
-//        var query = client.query('CREATE TABLE pg_equipment ( \
-//            id serial PRIMARY KEY,\
-//                agent varchar (400) NOT NULL, \
-//                time int NOT NULL, \
-//                date date NOT NULL \
-//            );'
-//        );
-//        query.on('end', function() {
-//            console.log("sddd");
-//            client.end()
-//            res.send('table created');
-//        });
-//    });
-//
-//});
+app.get('/install', function(req, res){
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+
+        var query = client.query('CREATE TABLE sets ( \
+                agent varchar(400) PRIMARY KEY,\
+                number int \
+            );'
+        );
+        query.on('end', function() {
+            console.log("sddd");
+            client.end()
+            res.send('table created');
+        });
+    });
+
+});
+
+app.get('/update/:agent', function(req, res){
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+        pg.connect(process.env.DATABASE_URL, function(err, client) {
+            var query = client.query('SELECT * FROM sets WHERE agent = "%s"' ,req.param('agent'));
+            query.on('end', function(result) {
+                if (!result) {
+                    return res.send('No data found');
+                }
+                else {
+                    res.send(result +"\n" + result.rowCount + ' rows were received');
+                }
+            });
+        });
+    });
+});
+
+app.get('/res/:agent', function(req, res){
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+        pg.connect(process.env.DATABASE_URL, function(err, client) {
+            var query = client.query('SELECT * FROM pg_equipment WHERE agent = "%s"' ,req.param('agent'));
+            query.on('end', function(result) {
+                if (!result) {
+                    return res.send('No data found');
+                    var query = client.query('INSERT INTO sets(agent,1) VALUES($1)', [agent]);
+                }
+                else {
+                    res.send(result +"\n" + result.rowCount + ' rows were received');
+                }
+            });
+        });
+    });
+});
+
+app.get('/sets/:agent', function(req, res){
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+        pg.connect(process.env.DATABASE_URL, function(err, client) {
+            var query = client.query('SELECT * FROM sets WHERE agent = "%s"' ,req.param('agent'));
+            query.on('end', function(result) {
+                if (!result) {
+                    return res.send('No data found');
+                }
+                else {
+                    res.send(result +"\n" + result.rowCount + ' rows were received');
+                }
+            });
+        });
+
+    });
+});
 
 
 var port = Number(process.env.PORT || 5000);
